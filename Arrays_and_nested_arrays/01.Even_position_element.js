01bf98cfa8e06da789f9ef4ccaf4942d4f8c4e08
Nikolay37731 <anikolai3773@gmail.com>
2024-12-03 07:27:53 +0200

    Delete 01.Even_position_element.js

--- Arrays_and_nested_arrays/01.Even_position_element.js
+++
@@ -1,9 +0,0 @@
-function Solve(input){
-    let numbers = [];
-    for (let i = 0; i < input.length; i++) {
-        if (i % 2 === 0) {
-            numbers.push(input[i]);
-        }
-    }
-    return numbers;
-}
