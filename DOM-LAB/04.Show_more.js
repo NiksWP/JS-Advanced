846098f8cace7c8d501f1824434551e2112ebafa
Nikolay37731 <anikolai3773@gmail.com>
2024-12-07 20:57:58 +0200

    Solving problem: Show more

---
+++ DOM-LAB/04.Show_more.js
@@ -0,0 +1,6 @@
+function showText() {
+    let moreTextElement = document.getElementById('text');
+    let moreButton = document.getElementById('more');
+    moreButton.style.display = 'none';
+    moreTextElement.style.display = '';
+}
