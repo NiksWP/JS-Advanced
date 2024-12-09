b57174a45c3a8aefc485ec75a295005adff79914
Nikolay37731 <anikolai3773@gmail.com>
2024-12-09 10:08:01 +0200

    Solving problem: Search in list

---
+++ DOM-Exercise/04.Search_in_list.js
@@ -0,0 +1,24 @@
+function search() {
+    let townsElements = document.querySelectorAll('ul li');
+ 
+    for (const town of townsElements) {
+          town.style.fontWeight = 'normal';
+          town.style.textDecoration = 'none';
+    }
+ 
+    let searching = document.querySelector('input[type=text]').value;
+    let regex = new RegExp(searching);
+ 
+    let sum = 0;
+    for (const town of townsElements) {
+       if (regex.test(town.textContent)) {
+          town.style.fontWeight = 'bold';
+          town.style.textDecoration = 'underline';
+          sum++;
+       }
+    }
+ 
+    let resultElement = document.getElementById('result');
+    resultElement.textContent = `${Number(sum)} matches found`
+}
+ 
