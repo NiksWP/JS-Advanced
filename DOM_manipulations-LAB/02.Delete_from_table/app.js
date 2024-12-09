e4a18803129421b8e010e2f18c59c68dd40146e1
Nikolay37731 <anikolai3773@gmail.com>
2024-12-09 23:52:42 +0200

    Solving problem: Delete from table

---
+++ DOM_manipulations-LAB/02.Delete_from_table/app.js
@@ -0,0 +1,17 @@
+function deleteByEmail() {
+    const inputElement = document.querySelector("input[name=email]");
+    const resultELement = document.getElementById("result");
+    const trElements = document.querySelectorAll('#customers tbody tr');
+
+    const resultRow =  Array.from(trElements).find(tr => tr.getElementsByTagName('td')[1]
+    .textContent === inputElement.value)
+    if (resultRow) {
+        resultRow.remove();
+        resultELement.textContent = 'Deleted.';
+    }
+    else{
+        resultELement.textContent = 'Not found.';
+    }
+
+    inputElement.value = '';
+}
